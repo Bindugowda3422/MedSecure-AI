210d415e66b56ff680a389e90be1297a4bd8d6ed
Shivakumarcr7 <patilshivakumarcr7@gmail.com>
2026-08-15 23:10:44 +0530

    Add MedSecure AI application and blockchain implementation

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,33 @@
+﻿import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+
+  networks: {
+    hardhat: {},
+
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+
+    sepolia: {
+      url: process.env.BLOCKCHAIN_RPC_URL || "",
+      accounts: process.env.BLOCKCHAIN_PRIVATE_KEY
+        ? [process.env.BLOCKCHAIN_PRIVATE_KEY]
+        : [],
+    },
+  },
+
+  paths: {
+    sources: "./blockchain/contracts",
+    tests: "./blockchain/test",
+    cache: "./blockchain/cache",
+    artifacts: "./blockchain/artifacts",
+  },
+};
+
+export default config;
